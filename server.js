13450fb58c97caed132cd338b8b72c16329860ae
hilmi <hilmi.hilmiramadani2110@gmail.com>
2024-01-17 14:15:29 +0700

    quiz api

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+const quizRoute = require('./router/quiz');
+const jobsheetRoute = require('./router/jobsheet');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require('./models')
+db.sequelize.sync()
+
+app.get('/', (req, res) => {
+    res.send('Hilmi here...');
+});
+
+app.use('/api/quizzes', quizRoute)
+app.use('/api/jobsheet', jobsheetRoute)
+
+app.listen(port, () => console.log(`sedang berjalan di port http://localhost:${port}!`));
